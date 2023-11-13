51e206359277a8bdbaf5f44f3a211092c9da954f
Nahuel Chamorro <118007578+nahucham18@users.noreply.github.com>
2023-11-13 20:42:27 -0300

    Cambio Principal

--- programaApellidos.php
+++ programaApellidos.php
@@ -15,6 +15,7 @@ include_once("wordix.php");
 /***** DEFINICION DE FUNCIONES ********/
 /**************************************/
 
+
 /**
  * Obtiene una colección de palabras
  * @return array
@@ -31,7 +32,39 @@ function cargarColeccionPalabras()
     return ($coleccionPalabras);
 }
 
-/*algo ****COMPLETAR aca ok***** */
+/**
+ * Buscar a la palabra segun el numero que eligio el usuario
+ * @param $arrayPalabras array
+ * @param $numPalabra int
+ * @return string
+ */
+function buscarPalabra($numPalabra, $arrayPalabras){
+    if(isset($arrayPalabras[$numPalabra])){
+        return $arrayPalabras[$numPalabra];
+    }else{
+        return "No existe esa palabra";
+    }
+}
+
+/**
+ * Busca si el jugador ya uso la palabra elegida
+ * @param $coleccionPartidas array
+ * @param $nombre string
+ * @param $palabra string
+ * @return boolean
+ */
+function buscarPalabraRepetida($coleccionPartidas, $nombre,$palabra){
+    $existe = false;
+    foreach($coleccionPartidas as $item){
+        if($item['jugador'] === $nombre && $item['palabraWordix'] === $palabra){
+            $existe = true;
+            break;
+        }        
+    }
+    return $existe;
+}
+
+/* ****COMPLETAR***** */
 
 
 
@@ -41,38 +74,78 @@ function cargarColeccionPalabras()
 
 //Declaración de variables:
 
+$coleccionPartidas = [];
 
 //Inicialización de variables:
 
 
 //Proceso:
 
-$partida = jugarWordix("MELON", strtolower("MaJo"));
 //print_r($partida);
 //imprimirResultado($partida);
 
 
-
-/*
-do {
-    $opcion = ...;
-
+do 
+{
+    echo "\n";
+    echo '1) Jugar al wordix con una palabra elegida ';
+    echo "\n";
+    echo '2) Jugar al wordix con una palabra aleatoria ';
+    echo "\n";
+    echo '3) Mostrar una partida ';
+    echo "\n";
+    echo '4) Mostrar la primer partida ganadora ';
+    echo "\n";
+    echo '5) Mostrar resumen de Jugador ';
+    echo "\n";
+    echo '6) Mostrar listado de partidas ordenadas por jugador y por palabra ';
+    echo "\n";
+    echo '7) Agregar una palabra de 5 letras a Wordix ';
+    echo "\n";
+    echo '8) salir';
+    echo "\n";
+    echo "Elegir una opcion:";
+    $opcion = trim(fgets(STDIN));
     
     switch ($opcion) {
-        case 1: 
-            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 1
+        case 0:
 
+            break;
+        case 1: 
+            $arrayPalabras = cargarColeccionPalabras();
+            echo "el array es";
+            print_r($arrayPalabras);
+            echo "Ingrese su nombre:";
+            $nombre = trim(fgets(STDIN));
+            do{
+                echo "Ingrese un numero entre 1 y 5";
+                $num = trim(fgets(STDIN));
+                $palabraBuscada = buscarPalabra($num,$arrayPalabras);
+                $palabraRepetida = buscarPalabraRepetida($coleccionPartidas,$nombre,$palabraBuscada);
+                if($palabraRepetida){
+                    echo "La palabra ya fue usada por $nombre";
+                }
+            }while($palabraRepetida === true);
+
+            $partida = jugarWordix("$palabraBuscada", strtolower($nombre));
+            array_push($coleccionPartidas, $partida);
+            // print_r($coleccionPartidas);
+            // $numPalabra = solicitarNumeroEntre($min, $max);
             break;
         case 2: 
-            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 2
-
+            echo 'completar qué secuencia de pasos ejecutar si el usuario elige la opción 2';
+            
             break;
         case 3: 
-            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 3
 
             break;
-        
-            //...
+        case 8:
+            echo ' Quiere cerrar el juego? Y/N';
+            $resp = trim(fgets(STDIN));
+            if($resp === 'n'){
+                $opcion = 0;
+            }else $opcion = 8;
+            break;
+
     }
-} while ($opcion != X);
-*/
+} while ($opcion !== 8 );
